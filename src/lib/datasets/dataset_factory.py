aa6fc135bbff49cda6c1d179adfbe45452635b33
jizefeng0810 <jizefeng0810@163.com>
2022-03-15 13:43:13 +0800

    update

--- src/lib/datasets/dataset_factory.py
+++ src/lib/datasets/dataset_factory.py
@@ -7,48 +7,14 @@ from .sample.exdet import EXDetDataset
 from .sample.ctdet import CTDetDataset
 from .sample.multi_pose import MultiPoseDataset
 
-from .dataset.coco import COCO
-from .dataset.pascal import PascalVOC
-from .dataset.kitti import KITTI
-from .dataset.coco_hp import COCOHP
 from .dataset.cityscapes import Cityscapes
 from .dataset.foggy_cityscapes import FoggyCityscapes
-from .dataset.kitti_2d import KITTI2D
-from .dataset.fake_kitti_2d import Fake_KITTI2D
 from .dataset.fake_cityscapes import Fake_Cityscapes
-from .dataset.cityscapes_caronly import Cityscapes_CarOnly
-from .dataset.bdd_daytime import BDD_Daytime
-from .dataset.bdd_night import BDD_Night
-from .dataset.fake_bdd_daytime import Fake_BDD_Daytime
-from .dataset.fake_sim10k import Fake_Sim10K
-from .dataset.sim10k import Sim10K
-from .dataset.bdd_d2n_day import BDD_D2N_DAY
-from .dataset.bdd_d2n_night import BDD_D2N_NIGHT
-from .dataset.fake_bdd_d2n_day import FAKE_BDD_D2N_DAY
-from .dataset.bdd_daytime_8cls import BDD_Daytime_8CLS
-from .dataset.fake_cityscapes2bdd import Fake_Cityscapes2BDD
 
 dataset_factory = {
-  'coco': COCO,
-  'pascal': PascalVOC,
-  'kitti': KITTI,
-  'coco_hp': COCOHP,
   'cityscapes': Cityscapes,
   'foggy_cityscapes': FoggyCityscapes,
-  'kitti_2d': KITTI2D,
-  'fake_kitti_2d': Fake_KITTI2D,
   'fake_cityscapes': Fake_Cityscapes,
-  'cityscapes_car_only': Cityscapes_CarOnly,
-  'bdd_daytime': BDD_Daytime,
-  'bdd_night': BDD_Night,
-  'fake_bdd_daytime': Fake_BDD_Daytime,
-  'sim10k': Sim10K,
-  'fake_sim10k': Fake_Sim10K,
-  'bdd_d2n_day': BDD_D2N_DAY,
-  'bdd_d2n_night': BDD_D2N_NIGHT,
-  'fake_bdd_d2n_day': FAKE_BDD_D2N_DAY,
-  'bdd_daytime_8cls': BDD_Daytime_8CLS,
-  'fake_cityscapes2bdd': Fake_Cityscapes2BDD,
 }
 
 _sample_factory = {
